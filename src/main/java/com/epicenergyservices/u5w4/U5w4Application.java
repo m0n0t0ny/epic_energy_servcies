d8ec2a6d9c90d3389f355bf5cae08c1b5bb53999
Antonio Bertuccio <anto.bertu@gmail.com>
2024-02-26 11:34:23 +0100

    first commit

---
+++ src/main/java/com/epicenergyservices/u5w4/U5w4Application.java
@@ -0,0 +1,13 @@
+package com.epicenergyservices.u5w4;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class U5w4Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(U5w4Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/epicenergyservices/u5w4/U5w4ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.epicenergyservices.u5w4;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class U5w4ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
