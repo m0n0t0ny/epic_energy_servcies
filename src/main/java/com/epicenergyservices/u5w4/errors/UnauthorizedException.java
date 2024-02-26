d008b05f01464f12752440d8c24bd13684ab9453
Antonio Bertuccio <anto.bertu@gmail.com>
2024-02-26 18:06:40 +0100

    Add UnauthorizedException

---
+++ src/main/java/com/epicenergyservices/u5w4/errors/UnauthorizedException.java
@@ -0,0 +1,7 @@
+package com.epicenergyservices.u5w4.errors;
+
+public class UnauthorizedException extends RuntimeException {
+  public UnauthorizedException(String message) {
+    super(message);
+  }
+}
