1a7053d5ba1a652db78cf7c3fc5a73c39bcee138
kavidiuk <kaviduwarnakulasuriya@gmail.com>
2024-02-26 14:40:31 +0100

    add User DTO

---
+++ src/main/java/com/epicenergyservices/u5w4/entities/UserDTO.java
@@ -0,0 +1,33 @@
+package com.epicenergyservices.u5w4.entities;
+
+import org.aspectj.weaver.ast.Var;
+import org.hibernate.type.descriptor.jdbc.VarcharJdbcType;
+
+import javax.management.relation.Role;
+import java.util.UUID;
+
+public class UserDTO {
+    private UUID id;
+    private String username;
+    private String email;
+    private String password;
+    private String name;
+    private String surname;
+    private String avatar;
+    private Role role;
+
+    public UserDTO(UUID id, String username, String email, String password, String name, String surname, String avatar, Role role){
+        this.id = id;
+        this.username = username;
+        this.email = email;
+        this.password = password;
+        this.name = name;
+        this.surname = surname;
+        this.avatar = avatar;
+        this.role = role;
+    }
+    public enum Role {
+        Admin,
+        User
+    }
+}
