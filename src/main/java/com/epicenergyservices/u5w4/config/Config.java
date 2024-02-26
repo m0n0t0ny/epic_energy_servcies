4625e23ca982cb91793f67169994ecfea84b25f5
Antonio Bertuccio <anto.bertu@gmail.com>
2024-02-26 18:52:46 +0100

    Add new Config class with security configurations and JWT filter.

---
+++ src/main/java/com/epicenergyservices/u5w4/config/Config.java
@@ -0,0 +1,57 @@
+package com.epicenergyservices.u5w4.config;
+
+import com.epicenergyservices.u5w4.security.JWTFilter;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+
+@Configuration
+@EnableWebSecurity
+@EnableMethodSecurity
+public class Config {
+  @Autowired
+  private JWTFilter jwtFilter;
+
+  @Bean
+  SecurityFilterChain securityFilterChain(HttpSecurity httpSecurity) throws Exception {
+    httpSecurity.formLogin(AbstractHttpConfigurer::disable);
+    httpSecurity.csrf(AbstractHttpConfigurer::disable);
+    httpSecurity.sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS));
+    httpSecurity.cors(Customizer.withDefaults());
+
+    httpSecurity.addFilterBefore(jwtFilter, UsernamePasswordAuthenticationFilter.class);
+
+    httpSecurity.authorizeHttpRequests(request -> request.requestMatchers("/**").permitAll());
+
+    return httpSecurity.build();
+  }
+
+  @Bean
+  PasswordEncoder getBCrypt() {
+    return new BCryptPasswordEncoder(12);
+  }
+
+//  @Bean
+//  CorsConfigurationSource corsConfigurationSource() {
+//    CorsConfiguration config = new CorsConfiguration();
+//
+//    config.setAllowedOrigins(Arrays.asList("http://localhost:3000", "https://mywonderfulfrontend.com"));
+//    config.setAllowedMethods(Arrays.asList("*"));
+//    config.setAllowedHeaders(Arrays.asList("*"));
+//
+//    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+//    source.registerCorsConfiguration("/**", config);
+//
+//    return source;
+//  }
+}
