f3b4fdf645105ad4d3fa1c7db9723ce24e38d710
Asura0107 <mircoorosio@gmail.com>
2024-02-27 18:48:46 +0100

    aggiunto address

--- src/main/java/com/epicenergyservices/u5w4/entities/Addrress.java
+++ src/main/java/com/epicenergyservices/u5w4/entities/Addrress.java
@@ -1,8 +1,7 @@
 package com.epicenergyservices.u5w4.entities;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.Id;
+import com.epicenergyservices.u5w4.enums.AddressType;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -14,13 +13,39 @@ import java.util.UUID;
 @Getter
 @Setter
 @NoArgsConstructor
-@AllArgsConstructor
 public class Addrress {
     @Id
     @GeneratedValue
     private UUID id;
-//    private String street;
-//    private String civic_number;
-//    private String location;
-//    private String cap
+    private String street;
+    private String civic_number;
+    private String location;
+    private String postal_code;
+    @ManyToOne
+    @JoinColumn(name = "townhall_id")
+    private TownHall townHall;
+    @Enumerated(EnumType.STRING)
+    private AddressType addressType;
+
+    public Addrress(String street, String civic_number, String location, String postal_code, TownHall townHall, AddressType addressType) {
+        this.street = street;
+        this.civic_number = civic_number;
+        this.location = location;
+        this.postal_code = postal_code;
+        this.townHall = townHall;
+        this.addressType = addressType;
+    }
+
+    @Override
+    public String toString() {
+        return "Addrress{" +
+                "id=" + id +
+                ", street='" + street + '\'' +
+                ", civic_number='" + civic_number + '\'' +
+                ", location='" + location + '\'' +
+                ", postal_code='" + postal_code + '\'' +
+                ", townHall=" + townHall +
+                ", addressType=" + addressType +
+                '}';
+    }
 }
